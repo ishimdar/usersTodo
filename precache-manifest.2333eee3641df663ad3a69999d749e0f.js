9b404493755aeaaa2a6c9d21232fd6e2615326b1
ishimdar <ishimdar.ahamad@gmail.com>
2024-04-14 17:04:11 +0530

    Updates

---
+++ precache-manifest.2333eee3641df663ad3a69999d749e0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d26eaa37fe048deb046",
+    "url": "/usersTodo/static/css/main.14a3fe43.chunk.css"
+  },
+  {
+    "revision": "4d26eaa37fe048deb046",
+    "url": "/usersTodo/static/js/main.3b48013e.chunk.js"
+  },
+  {
+    "revision": "dc074905787db531d1b2",
+    "url": "/usersTodo/static/js/runtime~main.ae780965.js"
+  },
+  {
+    "revision": "997dc7f72c4c5a092a1c",
+    "url": "/usersTodo/static/js/2.54c47613.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/usersTodo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "afa82e92633dc1cbb3c86b657e0ba3b4",
+    "url": "/usersTodo/index.html"
+  }
+];
